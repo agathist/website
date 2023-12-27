434f53d45422e21f36e0b874afecc9cb40620219
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2023-12-26 20:25:13 -0800

    update teammates handling

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,27 @@
-import { defineCollection } from "astro/content/runtime";
-import { z } from "astro/zod";
+import { defineCollection } from 'astro:content'
+import { z } from 'astro/zod'
 
 const servicesCollection = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
     order: z.number(),
-    published: z.boolean(),
+    published: z.boolean().default(false),
   }),
-});
+})
 
 const teamCollection = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     name: z.string(),
     role: z.string(),
     imgUrl: z.string().optional(),
     order: z.number(),
+    published: z.boolean().default(false),
   }),
-});
+})
 
 export const collections = {
   services: servicesCollection,
   team: teamCollection,
-};
+}
