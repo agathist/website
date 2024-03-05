06a40f176e40955ae70ee07e125cb5323ced833b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-03-05 08:32:49 -0800

    update button styles to use aria-disabled selector instead

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,11 +7,6 @@ type ButtonProps = {
   type?: HTMLButtonElement['type']
 }
 
-const DISABLED_STYLES = {
-  opacity: 0.8,
-  cursor: 'not-allowed',
-}
-
 export function Button({
   children,
   disabled = false,
@@ -19,25 +14,20 @@ export function Button({
   type = 'button',
   ...rest
 }: ButtonProps) {
-  const handleClick = React.useCallback(() => {
+  const handleClick = () => {
     if (disabled) return
-
     onClick?.()
-  }, [disabled, onClick])
+  }
 
   return (
     <button
       /**
        * It's more accessible to use aria-disabled than the disabled attribute,
        * according to https://css-tricks.com/making-disabled-buttons-more-inclusive
-       *
-       * Seems that using the `disabled` attr will hide it from screen readers.
-       * This does mean we need some style workarounds though
        */
       aria-disabled={disabled}
-      className="relative rounded bg-gradient-to-r from-pink-500 via-rose-500 to-orange-400 bg-[length:115%] px-4 py-2 transition-all active:top-[1px]"
+      className="relative rounded bg-gradient-to-r from-pink-500 via-rose-500 to-orange-400 bg-[length:115%] px-4 py-2 transition-all active:top-[1px] aria-disabled:cursor-not-allowed aria-disabled:opacity-80"
       onClick={handleClick}
-      style={disabled ? DISABLED_STYLES : {}}
       type={type}
       {...rest}
     >
