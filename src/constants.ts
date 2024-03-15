d1fdcbe252fffc418e2d950e98e53a8a818b5510
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-03-15 09:12:48 -0700

    make constant for top level nav items

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const TOP_LEVEL_NAV_ITEMS = [
+  { href: '/services', text: 'Services' },
+  { href: '/about', text: 'About' },
+  { href: '/blog', text: 'Blog' },
+  { href: '/projects', text: 'Projects' },
+  { href: '/contact', text: 'Contact Us' },
+] as const
